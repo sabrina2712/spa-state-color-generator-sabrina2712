b88d923d2c93d96a212cd61bad3ab671097659f9
sabrina2712 <mailroshni@gmail.com>
2020-04-20 08:29:18 -0400

    done without styling

--- src/components/colorgenerator.js
+++ src/components/colorgenerator.js
@@ -10,7 +10,7 @@ class ColorGenerator extends Component {
     var color = "";
     for (var i = 0; i < 3; i++) {
       var sub = Math.floor(Math.random() * 256).toString(16);
-      color += sub.length == 1 ? "0" + sub : sub;
+      color += sub.length === 1 ? "0" + sub : sub;
     }
     return "#" + color;
   };
@@ -21,21 +21,48 @@ class ColorGenerator extends Component {
       return { colors: [...this.state.colors, color] };
     });
   };
-  render() {
-    let colors = this.state.colors.map((color) => {
+
+  onRemoveColor = (color) => {
+    this.setState(() => {
+      let colors = this.state.colors.filter((c) => {
+        return c !== color;
+      });
+      return { colors: colors };
+    });
+  };
+
+  colorElements = (colors) => {
+    if (colors.length === 0) {
+      return this.emptyElement();
+    }
+    return colors.map((color) => {
       return (
-        <div className="color-component">
+        <div className="color-component" key={color}>
           <div className="color" style={{ backgroundColor: color }}>
             {color}
           </div>
-          <button>Remove</button>
+          <button
+            onClick={() => {
+              this.onRemoveColor(color);
+            }}
+          >
+            Remove
+          </button>
         </div>
       );
     });
+  };
+
+  emptyElement = () => {
+    return <div className="no-colors">There are No colors yet</div>;
+  };
+
+  render() {
+    let colors = this.colorElements(this.state.colors);
 
     return (
       <div>
-        <p className="colors-container">{colors} </p>
+        <div className="colors-container">{colors} </div>
         <button onClick={this.onColorCreate}>Create</button>
       </div>
     );
